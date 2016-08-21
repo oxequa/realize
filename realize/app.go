663e89e7352a47551ad9112ed3ac123746104489
alessio <asoseil@gmail.com>
2016-08-21 12:20:14 +0200

    different timestamp format for logs

--- realize/app.go
+++ realize/app.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"sync"
+	"time"
+	"log"
 )
 
 const (
@@ -22,6 +24,7 @@ var RedS = color.New(color.FgRed).SprintFunc()
 var BlueS = color.New(color.FgBlue).SprintFunc()
 var Blue = color.New(color.FgBlue, color.Bold).SprintFunc()
 var Yellow = color.New(color.FgYellow, color.Bold).SprintFunc()
+var YellowS = color.New(color.FgYellow).SprintFunc()
 var MagentaS = color.New(color.FgMagenta).SprintFunc()
 var Magenta = color.New(color.FgMagenta, color.Bold).SprintFunc()
 
@@ -29,9 +32,16 @@ var watcherIgnores = []string{"vendor", "bin"}
 var watcherExts = []string{".go"}
 var watcherPaths = []string{"/"}
 
+type logWriter struct {}
+
 // App struct contains the informations about realize
 type App struct {
 	Name, Version, Description, Author, Email string
+}
+
+func init(){
+	log.SetFlags(0)
+	log.SetOutput(new(logWriter))
 }
 
 // Init is an instance of app with default values
@@ -49,4 +59,8 @@ func Init() *App {
 func (app *App) Information() {
 	fmt.Println(Blue(app.Name) + " - " + Blue(app.Version))
 	fmt.Println(BlueS(app.Description) + "\n")
+}
+
+func (writer logWriter) Write(bytes []byte) (int, error) {
+	return fmt.Print(YellowS("[")+time.Now().UTC().Format("15:04:05") +YellowS("]")+string(bytes))
 }
