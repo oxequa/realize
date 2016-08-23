9045a1da74de6197df0deb094bb83143ce3da4a7
alessio <asoseil@gmail.com>
2016-08-23 16:32:46 +0200

    description layout fixed

--- realize/app.go
+++ realize/app.go
@@ -13,7 +13,7 @@ import (
 const (
 	AppName        = "Realize"
 	AppVersion     = "v1.0"
-	AppDescription = "A Golang build system with file watchers, output streams and live reload. Run, build and watch file changes with custom paths\n"
+	AppDescription = "A Golang build system with file watchers, output streams and live reload. Run, build and watch file changes with custom paths"
 	AppFile        = "realize.config.yaml"
 )
 
