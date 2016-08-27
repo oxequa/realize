3493b1b3f45b2c350b004aa9de8b62b79960a977
alessio <asoseil@gmail.com>
2016-08-27 15:21:12 +0200

    test moved to milestone 1.1

--- realize/config_test.go
+++ realize/config_test.go
@@ -7,10 +7,10 @@ import (
 
 var context *cli.Context
 
-func TestNew(t *testing.T) {
-	actual := New(context)
-	expected := &Config{file: AppFile, Version: AppVersion}
-	if actual == expected {
-		t.Errorf("Test failed, expected: '%s', got:  '%s'", expected, actual)
-	}
-}
+//func TestNew(t *testing.T) {
+//	actual := New(context)
+//	expected := &Config{file: AppFile, Version: AppVersion}
+//	if actual == expected {
+//		t.Errorf("Test failed, expected: '%s', got:  '%s'", expected, actual)
+//	}
+//}
