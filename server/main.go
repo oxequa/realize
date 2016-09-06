b2c38ef474939d9d01e0039383254bf675aa4e5e
alessio <asoseil@gmail.com>
2016-09-06 02:31:44 +0200

    bindata render

--- server/main.go
+++ server/main.go
@@ -3,13 +3,27 @@ package server
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
 	"net/http"
 )
 
+func render(c echo.Context, path string) error{
+	data, err := Asset(path)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+	rs := c.Response()
+	rs.Header().Set(echo.HeaderContentType, echo.MIMETextHTMLCharsetUTF8)
+	rs.WriteHeader(http.StatusOK)
+	rs.Write(data)
+	return nil
+}
+
 func init() {
 	e := echo.New()
+	e.Use(middleware.Gzip())
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hi Realize")
+		return render(c, "server/assets/index.html")
 	})
 	go e.Run(standard.New(":5000"))
 }
