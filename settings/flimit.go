586fd218f5469904363026680d9befa63e6a7f60
alessio <asoseil@gmail.com>
2016-11-21 23:10:55 +0100

    windows flimit fix

---
+++ settings/flimit.go
@@ -0,0 +1,16 @@
+// build !windows
+
+package settings
+
+import "syscall"
+
+// Flimit defines the max number of watched files
+func (s *Settings) Flimit() {
+	var rLimit syscall.Rlimit
+	rLimit.Max = s.Config.Flimit
+	rLimit.Cur = s.Config.Flimit
+	err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		s.Fatal("Error Setting Rlimit", err)
+	}
+}
---
+++ settings/flimit_windows.go
@@ -0,0 +1,8 @@
+package settings
+
+import "syscall"
+
+// Flimit defines the max number of watched files
+func (s *Settings) Flimit() {
+	return
+}
