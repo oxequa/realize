d2bc7e6fe56335096af96408aa04542fa25df5a5
alessio <asoseil@gmail.com>
2016-11-21 23:21:17 +0100

    outdate dependecies removed

--- server/main.go
+++ server/main.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	c "github.com/tockins/realize/settings"
--- settings/flimit_windows.go
+++ settings/flimit_windows.go
@@ -1,7 +1,5 @@
 package settings
 
-import "syscall"
-
 // Flimit defines the max number of watched files
 func (s *Settings) Flimit() {
 	return
