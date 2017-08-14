393c1450dcce6302e99b9036625de9d5717d13b1
alessio <asoseil@gmail.com>
2017-08-14 22:49:51 +0200

    server tests fixed

--- server/main_test.go
+++ server/main_test.go
@@ -1,9 +1,9 @@
 package server
 
 import (
-	"testing"
 	"github.com/tockins/realize/settings"
 	"net/http"
+	"testing"
 )
 
 func TestServer_Start(t *testing.T) {
@@ -11,41 +11,40 @@ func TestServer_Start(t *testing.T) {
 		Server: settings.Server{
 			Status: true,
 			Open:   false,
-			Host: "localhost",
-			Port: 5000,
+			Host:   "localhost",
+			Port:   5000,
 		},
 	}
 	server := Server{
-		Settings:  &s,
+		Settings: &s,
 	}
 	err := server.Start(nil)
-	if err != nil{
+	if err != nil {
 		t.Fatal(err)
 	}
 	host := "http://localhost:5000/"
 	urls := []string{
 		host,
-		host+"ws",
-		host+"assets/js/all.min.js",
-		host+"assets/css/app.css",
-		host+"app/components/settings/index.html",
-		host+"app/components/project/index.html",
-		host+"app/components/project/index.html",
-		host+"app/components/index.html",
-		host+"assets/img/svg/ic_settings_black_24px.svg",
-		host+"assets/img/svg/ic_fullscreen_black_24px.svg",
-		host+"assets/img/svg/ic_add_black_24px.svg",
-		host+"assets/img/svg/ic_keyboard_backspace_black_24px.svg",
-		host+"assets/img/svg/ic_error_black_48px.svg",
-		host+"assets/img/svg/ic_remove_black_24px.svg",
-		host+"assets/img/svg/logo.svg",
-		host+"assets/img/favicon-32x32.png",
-		host+"assets/img/svg/ic_swap_vertical_circle_black_48px.svg",
+		host + "assets/js/all.min.js",
+		host + "assets/css/app.css",
+		host + "app/components/settings/index.html",
+		host + "app/components/project/index.html",
+		host + "app/components/project/index.html",
+		host + "app/components/index.html",
+		host + "assets/img/svg/ic_settings_black_24px.svg",
+		host + "assets/img/svg/ic_fullscreen_black_24px.svg",
+		host + "assets/img/svg/ic_add_black_24px.svg",
+		host + "assets/img/svg/ic_keyboard_backspace_black_24px.svg",
+		host + "assets/img/svg/ic_error_black_48px.svg",
+		host + "assets/img/svg/ic_remove_black_24px.svg",
+		host + "assets/img/svg/logo.svg",
+		host + "assets/img/favicon-32x32.png",
+		host + "assets/img/svg/ic_swap_vertical_circle_black_48px.svg",
 	}
 	for _, elm := range urls {
 		resp, err := http.Get(elm)
 		if err != nil || resp.StatusCode != 200 {
-			t.Fatal(err)
+			t.Fatal(err, resp.StatusCode, elm)
 		}
 	}
 }
