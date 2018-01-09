7bd00b687b60bc5c86e8a7d4666059a69cf91b80
asoseil <asoseil@gmail.com>
2018-01-09 01:38:08 +0100

    server test removed

--- realize/server_test.go
+++ realize/server_test.go
@@ -6,34 +6,6 @@ import (
 	"testing"
 )
 
-func TestServer_Start(t *testing.T) {
-	// refactoring required
-	//s := Server{
-	//	Host: "localhost",
-	//	Port: 5000,
-	//}
-	//host := "http://localhost:5000/"
-	//urls := []string{
-	//	host,
-	//	host + "assets/js/all.min.js",
-	//	host + "assets/css/app.css",
-	//	host + "app/components/settings/index.html",
-	//	host + "app/components/project/index.html",
-	//	host + "app/components/project/index.html",
-	//	host + "app/components/index.html",
-	//}
-	//err := s.Start()
-	//if err != nil {
-	//	t.Fatal(err)
-	//}
-	//for _, elm := range urls {
-	//	resp, err := http.Get(elm)
-	//	if err != nil || resp.StatusCode != 200 {
-	//		t.Fatal(err, resp.StatusCode, elm)
-	//	}
-	//}
-}
-
 func TestServer_Open(t *testing.T) {
 	cmd := map[string]string{
 		"windows": "start",
